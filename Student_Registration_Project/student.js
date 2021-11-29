811f7b2392812c52ba7cad6d71b5608892761e44
Caglar <ahmetcmirza@gmail.com>
2021-11-29 22:06:45 +0100

    !deleteStudent - clearAllStudent

---
+++ Student_Registration_Project/student.js
@@ -0,0 +1,22 @@
+// -> class base
+// class Student1{
+//     constructorStudent(imageUrl, studentName, studentPath){
+//         console.log(this);
+//     }
+// }
+
+//-> functional base
+function Student(imageUrl, studentName, studentPath) {
+  // console.log(this);    // -> object
+  // console.log(self);    // -> window
+  this.imageUrl = imageUrl;
+  this.studentName = studentName;
+  this.studentPath = studentPath;
+  // console.log(this);
+}
+
+const std = new Student("url", "Caglar", "Fullstack");
+const std2 = new Student("url", "Mirza", "AWS");
+
+// console.log(std);
+// console.log(std2);
